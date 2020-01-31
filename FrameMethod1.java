d3e33f29782a32d4a723cf6187808e7e2c3043cb
Shane Cooke <60254954+shanecooke237@users.noreply.github.com>
2020-01-31 09:39:28 +0000

    Add files via upload

---
+++ FrameMethod1.java
@@ -0,0 +1,13 @@
+public static void Letters() {
+
+  char[] Frame = new char[7];
+  Frame[0] = 'A';
+  Frame[1] = 'X';
+  Frame[2] = 'N';
+  Frame[3] = 'Q';
+  Frame[4] = 'I';
+  Frame[5] = 'P';
+  Frame[6] = 'F';
+
+System.out.println(Frame);
+}
